562792dfcd1ed4880a80ba75c1af8f59576588fa
Ian Tolentino <165245557+iantolentino@users.noreply.github.com>
2026-03-04 21:18:50 +0800

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://l-vault.vercel.app"
+        "https://l-vault.vercel.app",
+        "https://linkvault-7ix0.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
